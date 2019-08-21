a625fc557fea9ccc28d0504dc4ef53dbfea25f90
Dmitriy <toropkind@gmail.com>
2019-08-21 11:40:24 +0300

    теперь записывается айди интересного места в его доукмент, в поле айди

--- functions/index.js
+++ functions/index.js
@@ -10,39 +10,39 @@ const FieldValue = require('firebase-admin').firestore.FieldValue;
 
 //Количество cityCount обновляется в зависимости от списка городов в стране
 exports.countCitiesDocumentsChange = functions.firestore.document('country/{countryId}/cities/{cityId}').onWrite((change, context) => {
-                                                                                                           
-                                                                                                           const countryId = context.params.countryId;
-                                                                                                           const countryRef = db.collection('country').doc(countryId)
-                                                                                                           
-                                                                                                           if (!change.before.exists) {
-                                                                                                           
-                                                                                                           // new document created : add one to count
-                                                                                                           countryRef.update({citiesCount: FieldValue.increment(1)});
-                                                                                                           console.log("%s numberOfDocs incremented by 1", countryId);
-                                                                                                           
-                                                                                                           } else if (change.before.exists && change.after.exists) {
-                                                                                                           
-                                                                                                           // updating existing document : Do nothing
-                                                                                                           
-                                                                                                           } else if (!change.after.exists) {
-                                                                                                           
-                                                                                                           // deleting document : subtract one from count
-                                                                                                           countryRef.update({citiesCount: FieldValue.increment(-1)});
-                                                                                                           console.log("%s numberOfDocs decremented by 1", countryId);
-                                                                                                           
-                                                                                                           }
-                                                                                                           
-                                                                                                           return 0;
-                                                                                                           });
+                                                                                                                 
+                                                                                                                 const countryId = context.params.countryId;
+                                                                                                                 const countryRef = db.collection('country').doc(countryId)
+                                                                                                                 
+                                                                                                                 if (!change.before.exists) {
+                                                                                                                 
+                                                                                                                 // new document created : add one to count
+                                                                                                                 countryRef.update({citiesCount: FieldValue.increment(1)});
+                                                                                                                 console.log("%s numberOfDocs incremented by 1", countryId);
+                                                                                                                 
+                                                                                                                 } else if (change.before.exists && change.after.exists) {
+                                                                                                                 
+                                                                                                                 // updating existing document : Do nothing
+                                                                                                                 
+                                                                                                                 } else if (!change.after.exists) {
+                                                                                                                 
+                                                                                                                 // deleting document : subtract one from count
+                                                                                                                 countryRef.update({citiesCount: FieldValue.increment(-1)});
+                                                                                                                 console.log("%s numberOfDocs decremented by 1", countryId);
+                                                                                                                 
+                                                                                                                 }
+                                                                                                                 
+                                                                                                                 return 0;
+                                                                                                                 });
 
 
 //Количество places обновляется в зависимости от количества метс
 exports.countPlacesDocumentsChange = functions.firestore.document('country/{countryId}/cities/{cityId}/places/{placeId}').onWrite((change, context) => {
                                                                                                                                   
                                                                                                                                   const placeId = context.params.placeId;
-//
-                                                                                                                                  const placeRef = db.collection('country/{countryId}/cities/{cityId}/places').doc(placeId)
-                                                                                                                                  console.log(`DEBUG Path of the subcollection place: ${placeRef.path}`);
+                                                                                                                                  //
+                                                                                                                                  //                                                                                                                                  const placeRef = db.collection('country/{countryId}/cities/{cityId}/places').doc(placeId)
+                                                                                                                                  //                                                                                                                                  console.log(`DEBUG Path of the subcollection place: ${placeRef.path}`);
                                                                                                                                   
                                                                                                                                   const countryId = context.params.countryId
                                                                                                                                   const countryRef = db.collection('country').doc(countryId);
@@ -52,9 +52,13 @@ exports.countPlacesDocumentsChange = functions.firestore.document('country/{coun
                                                                                                                                   const cityRef = countryRef.collection('cities').doc(cityId)
                                                                                                                                   console.log(`DEBUG Path of the subcollection city : ${cityRef.path}`);
                                                                                                                                   
+                                                                                                                                  const placeRef = cityRef.collection('places').doc(placeId)
+                                                                                                                                  console.log(`DEBUG Path of the subcollection place: ${placeRef.path}`);
+                                                                                                                                  
                                                                                                                                   if (!change.before.exists) {
                                                                                                                                   
                                                                                                                                   // new document created : add one to count
+                                                                                                                                  placeRef.set({id: placeId})
                                                                                                                                   countryRef.update({placesCount: FieldValue.increment(1)});
                                                                                                                                   cityRef.update({placesCount: FieldValue.increment(1)});
                                                                                                                                   console.log("DEBUG places count decremented by 1");
